40f137aea8b17de545f75287113f92fff386b54c
Navaneeth-Murali09 <navaneetha.murali@people10.com>
2022-08-23 17:43:46 +0530

    second commit

--- script2.js
+++ script2.js
@@ -1,95 +1,91 @@
-
-        let fn=false;
-        let ln=false;
-        let ag=false;
-        let gen=false;
-        let em=false;
-        let ph=false;
-        let details='';
-        //form details
-        let count=0;
+let fn=false;
+let ln=false;
+let ag=false;
+let gen=false;
+let em=false;
+let ph=false;
 
 
-        passanger_details();
+let first_name=document.getElementById('fname');
+let last_name=document.getElementById('lname');
+let age=document.getElementById('Age');
+let select=document.getElementById('gender');
+let val = select.options[select.selectedIndex].value;
+let mail=document.getElementById('mail');
+let number=document.getElementById('contact');
 
-        let first_name=document.getElementById('fname');
-        let last_name=document.getElementById('lname');
-        let age=document.getElementById('Age');
-        let select=document.getElementById('gender');
-        let val = select.options[select.selectedIndex].value;
-        let mail=document.getElementById('mail');
-        let number=document.getElementById('contact');
+const button=document.getElementById('button');
 
-        const button=document.getElementById('button');
+var name_regex = /^[a-zA-Z]{1,30}$/;
+var number_regex= /^[0-9]{10}$/;
+var age_regex= /^0?1[89]|0?[2-9][0-9]$/
+var mail_regex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 
-        var name_regex = /^[a-zA-Z]{1,30}$/;
-        var number_regex= /^[0-9]{10}$/;
-        var age_regex= /^0?1[89]|0?[2-9][0-9]$/
-        var mail_regex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+first_name.addEventListener('input', validate_first_name);
+last_name.addEventListener('input', validate_last_name);
+age.addEventListener('input',validate_age);
+mail.addEventListener('input',validate_mail);
+number.addEventListener('input',validate_number);
 
+confirm_button();
 
-        first_name.addEventListener('input', validate_first_name);
-        last_name.addEventListener('input', validate_last_name);
-        age.addEventListener('input',validate_age);
-        mail.addEventListener('input',validate_mail);
-        number.addEventListener('input',validate_number);
+function validate_first_name(){
+    console.log(first_name.value)
+    if(name_regex.test(first_name.value)){
+            validate(first_name);
+            fn=true;
+            confirm_button();
+    }
+    else{
+            error(first_name);
+            fn=false;
+}
+}
 
-        confirm_button();
-
-        function validate_first_name(){
-            console.log(first_name.value)
-            if(name_regex.test(first_name.value)){
-                validate(first_name);
-                fn=true;
-                confirm_button();
-            }
-            else{
-                error(first_name);
-                fn=false;
-            }
-        }
-
-        function validate_last_name(){
-            console.log(last_name.value)
-            if(name_regex.test(last_name.value)){
+function validate_last_name()
+{
+        console.log(last_name.value)
+        if(name_regex.test(last_name.value)){
                 validate(last_name);
                 ln=true;
                 confirm_button();
-            }
-            else{
+        }
+        else{
                 error(last_name);
                 ln=false;
-            }
         }
+}
 
-        function validate_age(){
-            console.log(age.value)
-            if(age_regex.test(age.value)){
+function validate_age()
+{
+        console.log(age.value)
+        if(age_regex.test(age.value)){
                 validate(age);
                 ag=true;
                 confirm_button();
-            }
-            else{
+        }
+        else{
                 error(age);
                 ag=false;
-            }
         }
+}
 
 
-        function validate_mail(){
-            console.log(mail.value)
-            if(mail_regex.test(mail.value)){
+function validate_mail()
+{
+        console.log(mail.value)
+        if(mail_regex.test(mail.value)){
                 validate(mail);
                 em=true;
                 confirm_button();
-            }
-            else{
+        }
+        else{
                 error(mail);
                 em=false;
-            }
         }
+}
 
-        function validate_number(){
+function validate_number(){
             console.log(number.value)
             if(number_regex.test(number.value)){
                 validate(number);
@@ -100,9 +96,9 @@
                 error(number);
                 ph=false;
             }
-        }
+}
 
-        function validate(val){
+function validate(val){
             val.style.borderColor = "green";
             
             //console.log(count);
@@ -112,137 +108,58 @@
             val.style.borderColor = "red";
 
             //console.log(count);
-        }
+}
 
-        function confirm_button(){
+function confirm_button(){
             if(fn===false && ln===false && ag===false  && em===false && ph===false){
 
                 button.disabled = true;
                 button.style.background=  "#d5dbd9";
-                button.style.color ="rgb(79, 108, 145)";
+                button.style.color ="rgb(255, 102, 0)";
             }
         else if(fn===true && ln===true && ag===true  && em===true && ph===true){
-                store_item(count);
+                store_item1();
                 button.disabled = false;
-                button.style.background=  "rgb(79, 108, 145)";
+                button.style.background=  "rgb(255, 102, 0)";
                 button.style.color ="#d5dbd9";
                 button.style.ponter="cursor";
-
             }
+}
 
-        }
-
-        function store_item(count){
-            localStorage.setItem("firstname", first_name.value);
-            localStorage.setItem("lastname", last_name.value);
-            localStorage.setItem("age", age.value);
-            localStorage.setItem("gender", val);
-            localStorage.setItem("email", mail.value);
-            localStorage.setItem("contact", number.value);
-        }
-
-
-
-        function add_pass(){
-            if (confirm("Save passanger Details")) {
-                count+=1;
-                store_item(count);
-                passanger_details();
-              } 
-        }
-
-        function  passanger_details(){
-            details =  `
-            <div class=form_display>
-                <form id="form" action="page3.html" class="passanger_detains">
-                    <div class="input_data">
-                    <label for="fname" class="firstname">First Name</label>
-                    <input type="text" id="fname">
-                    </div>
-
-                    <div class="input_data">
-                        <label for="lname" class="lastname">Last Name</label>
-                        <input type="text"  id="lname">
-                    </div>
-
-                    <div class="input_data">
-                        <label for="Age" class="age">Age</label>
-                        <input type="text" id="Age">
-                    </div>
-
-                    <div class="input_data">
-                    <label class="gender">Gender</label>
-                    <select id="gender">
-                        <option value="male">Male</option>
-                        <option value="female">Female</option>
-                    </select>
-
-                    </div>
-
-                    <div class="input_data">
-                        
-                        <label for="mail" class="email">Email</label>
-                        <input type="email" id="mail">
-
-                    </div>
-                    <div class="input_data">
-                        <label for="contact" class="phone">Phone Number</label>
-                        <input type="text" id="contact">
-                    
-                    </div>
+function store_item1(){
+            localStorage.setItem("firstname1", first_name.value);
+            localStorage.setItem("lastname1", last_name.value);
+            localStorage.setItem("age1", age.value);
+            localStorage.setItem("gender1", val);
+            localStorage.setItem("email1", mail.value);
+            localStorage.setItem("contact1", number.value);
+            localStorage.setItem("flag",0);
+}
 
 
+let first_name2=document.getElementById('fname2');
+let last_name2=document.getElementById('lname2');
+let age2=document.getElementById('Age2');
+select=document.getElementById('gender2');
+let val2 = select.options[select.selectedIndex].value;
 
-                    <div class="input_data">
-                    <input type="submit" value="Confirm" id="button" >
-                    </div>
-                    <div class="input_data">
-                        <input type="submit" value="Add Passanger" id="button" onclick=" add_pass()">
-                    </div>
-                </form>
-            </div>
-            `
-            document.querySelector("#input_details").innerHTML = details;
-        }
-
-
-
-
-
-
-        /*
-
-        function add_passenger(){
-            
-            //var main_div=document.getElementById("input_details");
-            var form = document.createElement("FORM");
-                form.setAttribute("id", "form");
-                form.setAttribute("class", "passanger_detains");
-                main_div.appendChild(form);
-            
-                var div1= document.createElement("DIV");
-                div1.setAttribute("class", "input_data");
-                document.getElementById("form").appendChild(div1);
-
-                var label1 = document.createElement("label");
-                label1.setAttribute("class", "firstname");
-                label1.innerHTML = "First Name";
-                document.getElementsByClassName("input_data").appendChild(label1);
-
-                var inp1 = document.createElement("INPUT");
-                inp1.setAttribute("type", "text");
-                inp1.setAttribute("class", "firstname");
-                document.getElementsByClassName("input_data").appendChild(inp1);
-
-                var label2 = document.createElement("label");
-                label2.setAttribute("class", "lastname");
-                label2.innerHTML = "Last Name";
-                document.getElementsByClassName("input_data").appendChild(label2);
-
-                var inp2 = document.createElement("INPUT");
-                inp2.setAttribute("type", "text");
-                inp2.setAttribute("class", "firstname");
-                document.getElementsByClassName("input_data").appendChild(inp2);
-                document.getElementById("input_details").appendChild(form);
-        }
-        */
+function add_pass()
+{
+    let pass=document.getElementById("add_passanger");
+    pass.style="display:true";
+    console.log(first_name2.value,last_name2.value, age2.value,val2);
+}
+function store_item2()
+{
+    
+    localStorage.setItem("firstname2", first_name2.value);
+    localStorage.setItem("lastname2", last_name2.value);
+    localStorage.setItem("age2", age2.value);
+    localStorage.setItem("gender2", val2);
+    localStorage.setItem("flag",1);
+}
+function submit_button_pressed()
+{
+    window.location.href="page3.html";
+}
+    
--- script3.js
+++ script3.js
@@ -1,19 +1,71 @@
 let flight_id=localStorage.getItem("flightid");
-let first_name=localStorage.getItem("firstname");
-let last_name=localStorage.getItem("lastname");
-let contact=localStorage.getItem("contact");
-let email=localStorage.getItem("email");
-let gender=localStorage.getItem("gender");
-let age=localStorage.getItem("age");
+let first_name=localStorage.getItem("firstname1");
+let last_name=localStorage.getItem("lastname1");
+let contact=localStorage.getItem("contact1");
+let email=localStorage.getItem("email1");
+let gender=localStorage.getItem("gender1");
+let age=localStorage.getItem("age1");
+let flag=localStorage.getItem("flag");
 
-const name=first_name+' '+last_name;
-
-flight_id-=1
-fetch('flights.json')
-.then(response => response.json())
-.then(data => display_value(data));
-
+let first_name2='',last_name2='',gender2='',age2='';
+let name2='';
 let content='';
+
+flight_id-=1;
+const name1=first_name+' '+last_name;
+if(flag==1)
+{
+    first_name2=localStorage.getItem("firstname2");
+    last_name2=localStorage.getItem("lastname2");
+    gender2=localStorage.getItem("gender2");
+    age2=localStorage.getItem("age2");
+    name2=first_name2+' '+last_name2;
+    fetch('flights.json')
+    .then(response => response.json())
+    .then(data => display_value2(data));
+}
+
+
+else{
+    fetch('flights.json')
+    .then(response => response.json())
+    .then(data => display_value(data));
+
+}
+
+function display_value2(data){
+    departure=new Date(data[flight_id].departTime);
+    arrival=new Date(data[flight_id].ArrivalTime);
+    content += 
+    `
+        <div class="details" >
+            <div class="flight_details">
+            <h1>Ticket Details</h1>
+                <h3>${data[flight_id].flightNum}</h3>
+                <h3>From: ${data[flight_id].origin}</h3>
+                <h3>To: ${data[flight_id].destination}</h3> 
+                <h3>Departure:<span>${departure.toUTCString()}</span></h3>
+                <h3>Arrival:<span>${arrival.toUTCString()}</span></h3>
+                <h3>Fare:<span>&#8377;${data[flight_id].price}</span></h3>
+            </div>
+            <div class= "passanger1_details">
+            <h1>Passanger1 Details</h1>
+                <h3 class="pname">Name: ${name1}</h3>
+                <h3 class="age">Age: ${age}</h3>
+                <h3 class="gender">Gender: ${gender}</h3>
+                <h3 class="contact">Contact No: ${contact}</h3>
+                <h3 class="email">Email: ${email}</h3>
+            </div>
+            <div class= "passanger2_details">
+            <h1>Passanger Details</h1>
+                <h3 class="pname">Name: ${name2}</h3>
+                <h3 class="age">Age: ${age2}</h3>
+                <h3 class="gender">Gender: ${gender2}</h3>
+            </div>
+
+    `
+    document.querySelector("#flight_details").innerHTML = content;
+}
 
 function display_value(data)
 {   
@@ -33,7 +85,7 @@ function display_value(data)
             </div>
             <div class= "passanger_details">
             <h1>Passanger Details</h1>
-                <h3 class="pname">Name: ${name}</h3>
+                <h3 class="pname">Name: ${name1}</h3>
                 <h3 class="age">Age: ${age}</h3>
                 <h3 class="gender">Gender: ${gender}</h3>
                 <h3 class="contact">Contact No: ${contact}</h3>
@@ -42,7 +94,6 @@ function display_value(data)
 
     `
 
-    
     document.querySelector("#flight_details").innerHTML = content;
 
 }
